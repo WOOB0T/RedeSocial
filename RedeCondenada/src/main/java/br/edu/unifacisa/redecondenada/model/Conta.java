374a07998da433a2e49c010d71015511dcd38e8b
Yuri Diego <contatoprofissional.ya@gmail.com>
2023-12-08 19:38:31 -0300

    Organizando o Back para funcionalidades...

--- RedeCondenada/src/main/java/br/edu/unifacisa/redecondenada/model/Conta.java
+++ RedeCondenada/src/main/java/br/edu/unifacisa/redecondenada/model/Conta.java
@@ -1,14 +1,40 @@
 package br.edu.unifacisa.redecondenada.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Conta {
 
     @Id
-    public String user;
-    public String email;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer id;
+    public String nomeDeUsuario;
     public String senha;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeDeUsuario() {
+        return nomeDeUsuario;
+    }
+
+    public void setNomeDeUsuario(String nomeDeUsuario) {
+        this.nomeDeUsuario = nomeDeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
